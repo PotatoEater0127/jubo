48b32fa845472e81a4811be99d93ee25a5939f0f
Henry Han <potatoEater0127@gmail.com>
2022-10-16 18:53:09 +0800

    feat: create patient list component

--- client/src/App.js
+++ client/src/App.js
@@ -1,29 +1,13 @@
-import { useEffect } from "react";
-import logo from "./logo.svg";
+import Box from "@mui/material/Box";
+import PatientList from "./components/PatientList";
 import "./App.css";
 
 function App() {
-  useEffect(() => {
-    fetch("/patients")
-      .then((res) => res.json())
-      .then((data) => console.log(data));
-  }, []);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Box>
+        <PatientList />
+      </Box>
     </div>
   );
 }
---
+++ client/src/components/PatientList/index.js
@@ -0,0 +1,33 @@
+import { useState, useEffect } from "react";
+import List from "@mui/material/List";
+import ListItem from "@mui/material/ListItem";
+import ListItemButton from "@mui/material/ListItemButton";
+import ListItemIcon from "@mui/material/ListItemIcon";
+import ListItemText from "@mui/material/ListItemText";
+import FaceIcon from "@mui/icons-material/Face";
+
+export default function PatientList() {
+  const [patients, setPatients] = useState([]);
+  useEffect(() => {
+    fetch("/patients")
+      .then((res) => res.json())
+      .then((patients) => setPatients(patients));
+  }, []);
+  return (
+    <List>
+      {patients.map((patient) => (
+        <ListItem key={patient?.id} disablePadding>
+          <ListItemButton>
+            <ListItemIcon>
+              <FaceIcon />
+            </ListItemIcon>
+            <ListItemText
+              primary={patient?.name}
+              secondary={`id: ${patient?.id}`}
+            />
+          </ListItemButton>
+        </ListItem>
+      ))}
+    </List>
+  );
+}
